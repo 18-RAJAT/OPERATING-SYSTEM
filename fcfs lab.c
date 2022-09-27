6e45aa8a2e94d39929ecbd32171aef8bfae1643d
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-09-27 19:25:08 +0530

    fcfs.c bug fix

--- fcfs lab.c
+++ fcfs lab.c
@@ -5,13 +5,13 @@ void fcfs()
 {
     int n,bt[1001],wt[1001],tat[1001],avgWt=0,avgTat=0;
     int i,j;
-    printf("Enter the number of processes :");
+    printf("Enter the number of processes : ");
     scanf("%d",&n);
 
-    printf("Enter the process of burst time :");
+    printf("Enter the process of burst time");
     for(i=0;i<n;++i)
     {
-        printf("\nP[%d]:",i+1);
+        printf("\nProcess[%d]:",i+1);
         scanf("%d",&bt[i]);
     }
     //first process
@@ -22,9 +22,8 @@ void fcfs()
     {
         wt[i]=0;
         for(j=0;j<i;++j)
-        {
-            wt[i]+=bt[j];
-        }
+        wt[i]+=bt[j];
+    }
         printf("\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time");
 
         //calculate turnaround time
@@ -33,13 +32,12 @@ void fcfs()
             tat[i]=wt[i]+bt[i];
             avgWt+=wt[i];
             avgTat+=tat[i];
-            printf("\nP[%d]\t\t%d\t\t%d\t\t%d",i+1,bt[i],wt[i],tat[i]);
+            printf("\nProcess[%d]\t\t%d\t\t%d\t\t%d",i+1,bt[i],wt[i],tat[i]);
         }
-        avgTat/=i;
         avgWt/=i;
+        avgTat/=i;
         printf("\nAverage Waiting Time : %d",avgWt);
         printf("\n\nAverage Turnaround Time : %d",avgTat);
-    }
 }
 
 int main()
