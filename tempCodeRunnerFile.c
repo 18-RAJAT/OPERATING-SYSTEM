d1dc6b90ef8a4174061e3edd89cf9506030adf76
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-09-27 20:17:03 +0530

    sjf.c

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,47 +1,64 @@
 #include<stdio.h>
 #include<conio.h>
 
-void fcfs()
+void sjf()
 {
-    int n,bt[1001],wt[1001],tat[1001],avgWt=0,avgTat=0;
-    int i,j;
-    printf("Enter the number of processes : ");
+    int bt[101],processNo[101],wt[101],tat[101],i,j,n,total=0,position,temp;
+
+    float avgWt,avgTat;
+    printf("Enter number of process:");
     scanf("%d",&n);
 
-    printf("Enter the process of burst time");
+    printf("Enter Burst Time: \n");
     for(i=0;i<n;++i)
     {
-        printf("\nProcess[%d]:",i+1);
+        printf("Process[%d]: ",i+1);
         scanf("%d",&bt[i]);
+        processNo[i]=i+1;
     }
-    //first process
-    wt[0]=0;
 
-    //waiting time 
+    for(i=0;i<n;++i)
+    {
+        position=i;
+        for(j=i+1;j<n;++j)
+        {
+            if(bt[j]<bt[position])
+            {
+                position=j;
+           
+            }
+        }
+        temp=bt[i];
+        bt[i]=bt[position];
+        bt[position]=temp;
+
+        temp=processNo[i];
+        processNo[i]=processNo[position];
+        processNo[position]=temp;
+    }wt[0]=0;
     for(i=1;i<n;++i)
     {
         wt[i]=0;
         for(j=0;j<i;++j)
         wt[i]+=bt[j];
+        total+=wt[i];
     }
-        printf("\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time");
+    avgWt=(float)total/n;total=0;
+    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
 
-        //calculate turnaround time
-        for(i=0;i<n;++i)
-        {
-            tat[i]=wt[i]+bt[i];
-            avgWt+=wt[i];
-            avgTat+=tat[i];
-            printf("\nProcess[%d]\t\t%d\t\t%d\t\t%d",i+1,bt[i],wt[i],tat[i]);
-        }
-        avgWt/=i;
-        avgTat/=i;
-        printf("\nAverage Waiting Time : %d",avgWt);
-        printf("\n\nAverage Turnaround Time : %d",avgTat);
+    for(i=0;i<n;++i)
+    {
+        tat[i]=bt[i]+wt[i];
+        total+=tat[i];
+         printf("\np%d\t\t  %d\t\t    %d\t\t\t%d",processNo[i],bt[i],wt[i],tat[i]);
+    }
+
+    avgTat=(float)total/n;
+    printf("\n\nAverage Waiting Time=%f",avgWt);
+    printf("\nAverage Turnaround Time=%f\n",avgTat);
 }
 
 int main()
 {
-    fcfs();
-    return 0;
+    sjf();
 }
