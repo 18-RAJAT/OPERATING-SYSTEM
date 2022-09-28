6380a88ad802a66aca97bd6e2fcb867536ef1759
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-09-28 10:15:12 +0530

    new prog

--- Round_Robin.c
+++ Round_Robin.c
@@ -21,7 +21,8 @@ int main()
 	for(i=1;i<n;i++)
 		if(max<bu[i])
 			max=bu[i];
-	for(j=0;j<(max/t)+1;j++)
+            int tp=max/t;
+	for(j=0;j<tp+1;j++)
 		for(i=0;i<n;i++)
 			if(bu[i]!=0)
 				if(bu[i]<=t)
@@ -41,8 +42,8 @@ int main()
 		att+=tat[i];
 		awt+=wa[i];
 	}
-	printf("\nThe Average Turnaround time is -- %f",att/n);
-	printf("\nThe Average Waiting time is --    %f",awt/n);
+	printf("\nThe Average Turnaround time is   %f",att/n);
+	printf("\nThe Average Waiting time is      %f",awt/n);
 	printf("\n");
 	printf("\n\tPROCESS\t BURST TIME \t WAITING TIME\tTURNAROUND TIME\n");
 	for(i=0;i<n;i++)
