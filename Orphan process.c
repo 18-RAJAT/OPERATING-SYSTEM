0082a8d7c9a19d661759a08c8a0e3743ac432403
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-10-11 12:43:57 +0530

    Orphan process

---
+++ Orphan process.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+
+int main()
+{
+    pid_t pid=fork();
+    if(pid>0)
+    {
+        printf("Parent process id=%d with child id=%d\n",getppid(),getpid());
+    }
+    else
+    {
+        sleep(20);
+        printf("Child process=%d and parent id=%d\n",getpid(),getppid());
+    }
+    return 0;
+}
