515ca8eb644afcf73290a3052e28d3216ab49441
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-12-20 01:09:26 +0530

    Scan Disk update

--- Scan_Disk_Scheduling.c
+++ Scan_Disk_Scheduling.c
@@ -1,78 +1,100 @@
-#define maxi 100
-#define mini 0
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-#include<math.h>
-
+#include <math.h>
 
 int main()
 {
-    int queue[1001];
-    int head,max,size,sum,temp;
-    int locationOfDiskHead;
-
-    printf("Enter the size of queue: ");
-    scanf("%d",&size);
-
-    printf("Enter the head position: ");
+    int queue1[20],queue2[20],queue3[20],seek=0;
+    int max,diff,head,n,m,temp1=0,temp2=0;
+    int i,j;
+    
+    float avg;
+    
+    printf("Enter the max range of the disk:\n ");
+    scanf("%d",&max);
+    
+    printf("Enter the initial head position:\n ");
     scanf("%d",&head);
-
-    printf("Enter the disks\n");
-    for(int i=0;i<size;++i)
+    
+    printf("Enter the size of queue requests:\n ");
+    scanf("%d",&n);
+    
+    printf("Enter the queue of disk positions:\n ");
+    for(i=1;i<=n;++i)
     {
-        scanf("%d",&queue[i]);
+    	scanf("%d",&m);
+    	{
+    		if(m>=head)
+    		{
+    			queue2[temp1]=m;
+    			temp1++;
+    		}
+    		else
+    		{
+    			queue3[temp2]=m;
+    			temp2++;
+    		}
+    	}
     }
-    queue[size]=head;
-    size++;
-
-    for(int i=0;i<size;++i)
-    {
-        for(int j=i;j<size;++j)
+    	//
+    	for(i=0;i<temp1-1;++i)
+    	{
+    		for(j=i+1;j<temp1;++j)
+    		{
+    			if(queue2[i]>queue2[j])
+    			{
+    				//sort
+    				m=queue2[i];
+    				queue2[i]=queue2[j];
+    				queue2[j]=m;
+    			}
+    		}
+    	}
+    	//
+    	for(i=0;i<temp2-1;++i)
+    	{
+    		for(j=i+1;j<temp2;++j)
+    		{
+    			if(queue3[i]<queue3[j])
+    			{
+    				//sort
+    				m=queue3[i];
+    				queue3[i]=queue3[j];
+    				queue3[j]=m;
+    			}
+    		}
+    	}
+        for(i=1,j=0;j<temp1;++i,++j)
         {
-            if(queue[i]>queue[j])
-            {
-                //sort the queue
-                temp=queue[i];
-                queue[i]=queue[j];
-                queue[j]=temp;
-            }
+            // for(int j=0;j<temp1;++j)
+            // {
+                queue1[i]=queue2[j];
+            // }
         }
-    }
-    max=queue[size-1];
-
-    //locate head in the queue
-    for(int i=0;i<size;++i)
-    {
-        if(head==queue[i])
+        queue1[i]=max;
+        queue1[i+1]=0;
+    	
+        for(i=temp1+3,j=0;j<temp2;++i,++j)
         {
-            locationOfDiskHead=i;
-            break;
+            // for(int j=0;j<temp2;++j)
+            // {
+                queue1[i]=queue3[j];
+            // }
         }
-    }
-    if(abs(head-mini)<=abs(head-maxi))
-    {
-        for(int i=locationOfDiskHead;i>=0;--i)
-        {
-            printf("%d -> ",queue[i]);
-        }
-        // printf("0 -> ");
-        for(int j=locationOfDiskHead+1;j<size;++j)
-        {
-            printf("%d -> ",queue[j]);
-        }
-    }
-    else
-    {
-        for(int j=locationOfDiskHead+1;j<size;++j)
-        {
-        	printf("%d -> ",queue[j]);
-        }
-        for(int j=locationOfDiskHead;j>=0;--j)
-        {
-        	printf("%d -> ",queue[j]);
-        }
-    }
-    sum=head+max;
-    printf("Movement of total cylinders %d",sum);
-    return 0;
+        queue1[0]=head;
+    	for(j=0;j<=n+1;++j)
+    	{
+    		diff=abs(queue1[j+1]-queue1[j]);
+    		seek+=diff;
+    		
+    		printf("Disk head moves from %d to %d with seek %d\n",queue1[j],queue1[j+1],diff);
+    	}
+    	
+    	printf("Total seek time %d\n",seek);
+    	
+    	avg=seek/n;
+    	printf("Avg seek time is %f\n",avg);
 }
+// Input: I/O requests - { 19, 80, 134, 11, 110, 23, 162, 64 }
+
+// Initial head position - 50
