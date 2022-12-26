6d6f4d76c0ae23bb7fbb8f6e6dc978d8cb6321c0
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-12-26 21:16:38 +0530

    Add all

--- sjf.c
+++ sjf.c
@@ -68,3 +68,71 @@ int main()
     printf("\n");
     printf("Avg tat time=%f",avgTat);
 }
+
+
+
+// #include<stdio.h>
+
+// int main(){
+
+// 	int n,tat[20], wt[20], bt[20],P[20],i,j, pos=-1, avgwt=0, avgtat=0;
+// 	printf("Enter no. of processes (max 20) : ");
+// 	scanf("%d",&n);
+
+// 	//Input CPU burst time for all processes
+// 	for(i=0;i<n;i++)
+// 	{
+// 		printf("P[%d]:",i);
+// 		scanf("%d", &bt[i]);
+// 		P[i]=i+1;
+// 	}
+
+// 	//Sort CPU burst time in ascending order using selection sort
+// 	for(i=0;i<n;i++)
+// 	{
+// 		pos=i;
+// 		for(j=i+1;j<n;j++)
+// 		{
+// 			if(bt[j]<bt[i])
+// 				pos=j;
+// 		}
+// 		int temp=P[i];
+// 		P[i]=P[pos];
+// 		P[pos]=temp;
+
+// 		temp=bt[i];
+// 		bt[i]=bt[pos];
+// 		bt[pos]=temp;
+// 	}
+
+// 	printf("Printing sorted array:");
+// 	for(i=0;i<n;i++)
+// 		printf(" %d --> %d",P[i],bt[i]);
+	
+// 	//Calculating waiting time
+// 	wt[0]=0;
+// 	for(i=1;i<n;i++)
+// 	{
+// 		wt[i]=0;
+// 		for(j=0;j<i;j++)
+// 			wt[i]+=bt[j];
+// 		avgwt+=wt[i];
+// 	}
+// 	avgwt/=n;
+// 	printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+// 	for(i=0;i<n;i++)
+//     	{
+// 		tat[i]=bt[i]+wt[i];     //calculate turnaround time
+// 	        avgtat+=tat[i];
+// 	        printf("\np[%d]\t\t  %d\t\t    %d\t\t\t%d",P[i],bt[i],wt[i],tat[i]);
+    	
+// 	}
+
+// 	avgtat/=n;
+// 	printf("\nAverage waiting time: %d", avgwt);
+// 	printf("\nAverage turn around time: %d\n\n", avgtat);
+	
+
+
+// 	return 0;
+// }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,77 +1,76 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main()
 {
-    int nFrame,n,frames[30],pages[30],cnt=0,time[30],flag1,flag2,i,j,pos,pageFault=0;
-    printf("Enter number of frames: ");
-    scanf("%d",&nFrame);
+	int n,i,qt,count=0,temp,f=0,bt[101],wt[101],tat[101],rt[101],ct[101];
+	float avgWt=0,avgTat=0;
 
-    printf("Enter number of pages: ");
-    scanf("%d",&n);
+	printf("Enter number of process: ");
+	scanf("%d",&n);
 
-    printf("Enter page numbers: ");
+	printf("Enter the burst time\n");
+	for(i=0;i<n;++i)
+	{
+		scanf("%d",&bt[i]);
+		rt[i]=bt[i];
+	}
+	printf("Time quantum: ");
+	scanf("%d",&qt);
 
-    for(i=0;i<n;++i)
-        scanf("%d",&pages[i]);
 
-    for(i=0;i<nFrame;++i)
-        frames[i]=-1;
+	while(1)
+	{
+		for(i=0,count=0;i<n;++i)
+		{
+			temp=qt;
+			if(rt[i]==0)
+			{
+				count++;
+				continue;
+			}
+			if(rt[i]>qt)
+			{
+				rt[i]-=qt;
+			}
+			else
+				if(rt[i]>=0)
+				{
+					temp=rt[i];
+					rt[i]=0;
+				}
+			f+=temp;
+			tat[i]=f;
+		}
+		if(n==count)
+		{
+			break;
+		}
+	}
+	printf("\nprocess\t\tBurst time\t\tTurnaround time\t\tWaiting time");
 
-    for(i=0;i<n;++i)
-    {
-        flag1=flag2=0;
-
-        for(j=0;j<nFrame;++j)
-        {
-            if(pages[i]==frames[j])
-            {
-                cnt++;
-                time[j]=cnt;
-                flag1=flag2=1;
-                break;
-            }
-        }
-
-        if(flag1==0)
-        {
-            for(j=0;j<nFrame;++j)
-            {
-                if(frames[j]==-1)
-                {
-                    cnt++;
-                    pageFault++;
-                    frames[j]=pages[i];
-                    time[j]=cnt;
-                    flag2=1;
-                    break;
-                }
-            }
-        }
-
-        if(flag2==0)
-        {
-            pos=0;
-            for(j=1;j<nFrame;++j)
-            {
-                if(time[j]<time[pos])
-                    pos=j;
-            }
-
-            cnt++;
-            pageFault++;
-            frames[pos]=pages[i];
-            time[pos]=cnt;
-        }
-
-        printf("\n");
-
-        for(j=0;j<nFrame;++j)
-            printf("%d\t",frames[j]);
-    }
-
-    printf("\n\nTotal Page Faults = %d",pageFault);
-    printf("\nMiss Ratio = %f",(pageFault/(1.0*n)));
-    printf("\nHit Ratio = %f",1-(pageFault/(1.0*n)));
-
-    return 0;
+	for(i=0;i<n;++i)
+	{
+		wt[i]=tat[i]-bt[i];
+		avgWt+=wt[i];
+		avgTat+=tat[i];
+		printf("\n%d\t\t%d\t\t\t\t%d\t\t\t%d",i+1,bt[i],tat[i],wt[i]);
+	}
+	avgWt/=n;
+	avgTat/=n;
+	printf("\nAverage waiting time=%f",avgWt);
+	printf("\nAverage turnaround time=%f",avgTat);
 }
+// Enter number of process: 3
+// Enter the burst time
+// 10
+// 5
+// 8
+// Time quantum: 2
+
+// process         Burst time              Turnaround time         Waiting time
+// 1               10                              23                      13
+// 2               5                               15                      10
+// 3               8                               21                      13
+// Average waiting time=12.000000
+// Average turnaround time=19.666666
