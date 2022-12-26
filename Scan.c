8da035344700a22cfa88b54f7a4c4fe09b0d7e81
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-12-27 00:07:28 +0530

    Add all

--- Scan.c
+++ Scan.c
@@ -1,50 +1,122 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// void swap(int disk[],int i,int j){
+//     int temp=disk[i];
+//     disk[i]=disk[j];
+//     disk[j]=temp;
+// }
+
+// int main(){
+//     int tracks,totaltracks;
+//     printf("Enter total tracks\n");
+//     scanf("%d",&totaltracks);
+    
+//     printf("Enter the number tracks on disk\n");
+//     scanf("%d",&tracks);
+    
+//     int disk[tracks];
+//     printf("Enter the tracks\n");
+//     for(int i=0;i<tracks;i++){
+//         scanf("%d",&disk[i]);
+//     }
+    
+//     int head,start=-1;
+//     printf("Enter the head of Read/Write head\n");
+//     scanf("%d",&head);
+    
+    
+//     //sorting
+//     for(int i=0;i<tracks;i++){
+//         int mn=disk[i];
+//         int ind=i;
+//         for(int j=i+1;j<tracks;j++){
+//             if(disk[j]<mn){
+//                 mn=disk[j];
+//                 ind=j;
+//             }
+//         }
+//         swap(disk,i,ind);
+//         if(disk[i]>=head && start<0)start=i-1;
+//     }
+    
+//     for(int i=start;i<tracks;i++){ //going forward only first
+//         printf("%d ",disk[i]);
+//     }
+//     for(int i=start-1;i>=0;i--){ // going backward only later
+//         printf("%d ",disk[i]);
+//     }
+//     printf("\nTotal tracks movement - %d\n",(totaltracks-head)+(totaltracks-disk[0]));
+// }
+
+
 #include<stdio.h>
 #include<stdlib.h>
 
-void swap(int disk[],int i,int j){
-    int temp=disk[i];
-    disk[i]=disk[j];
-    disk[j]=temp;
-}
-
-int main(){
-    int tracks,totaltracks;
-    printf("Enter total tracks\n");
-    scanf("%d",&totaltracks);
-    
-    printf("Enter the number tracks on disk\n");
-    scanf("%d",&tracks);
-    
-    int disk[tracks];
-    printf("Enter the tracks\n");
-    for(int i=0;i<tracks;i++){
-        scanf("%d",&disk[i]);
-    }
-    
-    int head,start=-1;
-    printf("Enter the head of Read/Write head\n");
+int main()
+{
+    int n, i, j, head, item[20], dst[20], limit;
+    int cylinders=0;
+    printf("Enter no. of locations:");
+    scanf("%d",&n);
+    printf("Enter position of head:");
     scanf("%d",&head);
-    
-    
-    //sorting
-    for(int i=0;i<tracks;i++){
-        int mn=disk[i];
-        int ind=i;
-        for(int j=i+1;j<tracks;j++){
-            if(disk[j]<mn){
-                mn=disk[j];
-                ind=j;
+    printf("Enter cylinder limit:");
+    scanf("%d",&limit);
+
+    printf("Enter elements of disk queue:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&item[i]);
+        dst[i]=(head-item[i]);
+    }
+    //Selection Sort
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(dst[j]>dst[i])
+            {
+                int temp=dst[j];
+                dst[j]=dst[i];
+                dst[i]=temp;
+
+                temp=item[i];
+                item[i]=item[j];
+                item[j]=temp;
             }
         }
-        swap(disk,i,ind);
-        if(disk[i]>=head && start<0)start=i-1;
     }
-    
-    for(int i=start;i<tracks;i++){ //going forward only first
-        printf("%d ",disk[i]);
+
+    for(i=0;i<n;i++)
+    {
+        if(item[i]>=head)
+        {
+            j=i;
+            break;
+        }
     }
-    for(int i=start-1;i>=0;i--){ // going backward only later
-        printf("%d ",disk[i]);
+
+    printf("j=%d", j);
+    printf("\n\nOrder of disk allocation is as follows:\n");
+    for(i=j;i<n;i++)
+    {
+        printf(" -> %d", item[i]);
+        cylinders+= abs(head-item[i]);
+        head=item[i];
+
     }
-    printf("\nTotal tracks movement - %d\n",(totaltracks-head)+(totaltracks-disk[0]));
+
+    //change
+    cylinders+= abs(limit-head) + limit ;
+    head =0;
+    for(i=j-1;i>=0;i--)
+    {
+        printf(" -> %d", item[i]);
+        cylinders+= abs(head-item[i]);
+        head=item[i];
+
+    }
+
+    printf("\n\nCylinder movement: %d\n\n", cylinders );
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,76 +1,79 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+
 
 int main()
 {
-	int n,i,qt,count=0,temp,f=0,bt[101],wt[101],tat[101],rt[101],ct[101];
-	float avgWt=0,avgTat=0;
-
-	printf("Enter number of process: ");
+	int n,at[n],bt[n],remBt[n],sq=0;
+	int timeQuantum;
+	printf("Enter the number of process: ");
 	scanf("%d",&n);
 
-	printf("Enter the burst time\n");
-	for(i=0;i<n;++i)
+	printf("Arrival Time: ");
+	for(int i=0;i<n;++i)
 	{
+		printf("at[%d] : ",i+1);
+		scanf("%d",&at[i]);
+	}
+
+	printf("Burst Time: ");
+	for(int i=0;i<n;++i)
+	{
+		printf("bt[%d] : ",i+1);
 		scanf("%d",&bt[i]);
-		rt[i]=bt[i];
+		remBt[i]=bt[i];
 	}
-	printf("Time quantum: ");
-	scanf("%d",&qt);
+	printf("Time Quantum: ");
+	scanf("%d",&timeQuantum);
+	int store=n;
+	int temp;
+	int ct=0;
+	int total=0;
 
+	float awt,atat;
+	
+	printf("Ans:\n");
+	printf("Process\t\tBurst Time\t\tCompletion Time\t\tTurnaround Time\t\tWaiting Time\n");
 
-	while(1)
+	for(int i=0,total=0;store!=0;)
 	{
-		for(i=0,count=0;i<n;++i)
+		if(remBt[i]<=timeQuantum && remBt[i]>0)
 		{
-			temp=qt;
-			if(rt[i]==0)
-			{
-				count++;
-				continue;
-			}
-			if(rt[i]>qt)
-			{
-				rt[i]-=qt;
-			}
-			else
-				if(rt[i]>=0)
-				{
-					temp=rt[i];
-					rt[i]=0;
-				}
-			f+=temp;
-			tat[i]=f;
+			ct+=remBt[i];
+			remBt[i]=0;
+			sq=1;
 		}
-		if(n==count)
+		else if(remBt[i]>0)
 		{
-			break;
+			remBt[i]-=timeQuantum;
+			ct+=timeQuantum;
+		}
+		if(remBt[i]==0 && sq==1)
+		{
+			store--;
+			printf("P[%d]\t\t%d\t\t%d\t\t%d\t\t%d\n",i+1,bt[i],ct,ct-at[i],ct-at[i]-bt[i]);
+			total+=ct-at[i]-bt[i];
+			sq=0;
+		}
+		if(i==n-1)
+		{
+			i=0;
+		}
+		else if(at[i+1]<=ct)
+		{
+			i++;
+		}
+		else
+		{
+			i=0;
 		}
 	}
-	printf("\nprocess\t\tBurst time\t\tTurnaround time\t\tWaiting time");
+	atat=(float)total/n;
+	printf("Average Turnaround Time: %f ",atat);
+	// return 0;
 
-	for(i=0;i<n;++i)
-	{
-		wt[i]=tat[i]-bt[i];
-		avgWt+=wt[i];
-		avgTat+=tat[i];
-		printf("\n%d\t\t%d\t\t\t\t%d\t\t\t%d",i+1,bt[i],tat[i],wt[i]);
-	}
-	avgWt/=n;
-	avgTat/=n;
-	printf("\nAverage waiting time=%f",avgWt);
-	printf("\nAverage turnaround time=%f",avgTat);
+	awt=(float)total/n;
+	printf("Average Waiting Time: %f ",awt);
+	return 0;
 }
-// Enter number of process: 3
-// Enter the burst time
-// 10
-// 5
-// 8
-// Time quantum: 2
-
-// process         Burst time              Turnaround time         Waiting time
-// 1               10                              23                      13
-// 2               5                               15                      10
-// 3               8                               21                      13
-// Average waiting time=12.000000
-// Average turnaround time=19.666666
